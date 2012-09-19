f9f714bcb8d2995c8bf078d36629c589033df3ac
V2905 (Conference ID) <v2905@pcmathsg03.maths.pwf.cam.ac.uk>
2012-09-19 16:27:57 +0100

    contour plot of the tracer

--- display_flow.m
+++ display_flow.m
@@ -8,9 +8,7 @@ clf;
 % Change the following lines to plot whatever you like during the simulation
 % Density
 surf(GXF(ii),GYF(jj),zeros(length(ii),length(jj))',TH(ii,jj,1)','EdgeColor','none'),view(0,90); shading interp; 
-% TODO: plot the dye
-hold on
-contour(GXF(ii),GYF(jj),TH(ii,jj,2)'); 
+
 set(gca,'FontName','Times','FontSize',14);
 xlabel('X');
 ylabel('Y');
@@ -19,6 +17,14 @@ caxis([0 1]);
 axis([GX(ii(1)) GX(ii(end)) GYF(jj(1)) GYF(jj(end))]);
 colorbar
 set(gca,'FontName','Times','FontSize',14);
+
+% TODO: plot the dye
+hold on
+[C,h] = contour(GXF(ii),GYF(jj),TH(ii,jj,2)',[0.1, 0.5, 0.8],'-k','LineWidth',2); 
+text_handle = clabel(C,h);
+set(text_handle,'FontSize',12)
+%,'BackgroundColor',[1 1 .6],'Edgecolor',[.7 .7 .7])
+
 
 % ******** End of User Input *********
 
